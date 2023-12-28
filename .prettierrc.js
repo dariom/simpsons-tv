b1d3d04dbb00d12df861a9090812f7387c91b9b5
Dario Mratovich <dario.mratovich@makerx.com.au>
2023-12-28 16:40:06 +0800

    Add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    singleQuote: true,
+    jsxSingleQuote: false,
+    semi: false,
+    tabWidth: 2,
+    trailingComma: 'all',
+    printWidth: 140,
+    endOfLine: 'lf',
+    arrowParens: 'always',
+  }
+  
