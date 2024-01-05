62dcd1a2768373bc23cf0c1538a14e9d61332adc
Dario Mratovich <dario.mratovich@makerx.com.au>
2024-01-05 11:02:19 +0800

    Use VLC playlists to coordinate playback
    Tidy up alt script

--- player-alt.py
+++ player-alt.py
@@ -5,7 +5,6 @@ import os
 import sys
 import RPi.GPIO as GPIO
 import vlc
-from pathlib import Path
 import time as time_ # Don't override time
 
 class VideoSelector:
@@ -16,79 +15,53 @@ class VideoSelector:
         self._currentVideoIndex = 0
 
         self._channels = []
+        self._videos = []
         for videoRoot, channels, files in os.walk(self._videoRoot):
             for channel in channels:
                 self._channels.append(channel)
+            for file in files:
+                filePath = os.path.join(videoRoot, file)
+                if (os.path.exists(filePath) and filePath.lower().endswith(self._videoFileExtension)):
+                    self._videos.append(filePath)
         self._channels.sort()
         if (len(self._channels) <= 0):
-            print(f"Directory '{videoRoot}' does not contain any sub-directories to be used as a 'channels'.")
+            print(f"Directory '{videoRoot}' does not contain any sub-directories to be used as a 'channel'.")
             quit()
-
-        self._load_current_channel_videos()
-
-    def _load_current_channel_videos(self):
-        channel = self.get_current_channel()
-        path = os.path.join(self._videoRoot, channel)
-
-        self._videos = []
-        for file in os.listdir(path):
-            filePath = os.path.join(path, file)
-            if (os.path.exists(filePath) and filePath.lower().endswith(self._videoFileExtension)):
-                self._videos.append(file)
         self._videos.sort()
         if (len(self._videos) <= 0):
-            print(f"Directory '{path}' does not contain any files with extension {self._videoFileExtension} to play back.")
+            print(f"The directories within '{videoRoot}' do not contain any files with extension {self._videoFileExtension} to play.")
             quit()
 
-    def get_current_video_name(self):
-        file = self._videos[self._currentVideoIndex]
-        return file[0:(len(file) - len(self._videoFileExtension))] # Strip file extension from file
-
-    def get_current_video(self):
-        channel = self.get_current_channel()
-        file = self._videos[self._currentVideoIndex]
-        return os.path.join(self._videoRoot, channel, file)
-    
     def get_current_channel(self):
         return self._channels[self._currentChannelIndex]
-    
-    def next_video(self):
-        self._currentVideoIndex += 1
-        if (self._currentVideoIndex >= len(self._videos)):
-            self._currentVideoIndex = 0
-            self.next_channel()
-        
-        return self.get_current_video()
     
     def next_channel(self):
         self._currentChannelIndex += 1
         if (self._currentChannelIndex >= len(self._channels)):
             self._currentChannelIndex = 0
+
+        # Find index of first video for the next channel
+        channel = self.get_current_channel()
+        channelPath = os.path.join(self._videoRoot, channel)
+        for videoIndex in range(len(self._videos)):
+            if (self._videos[videoIndex].startswith(channelPath)):
+                return videoIndex
         
-        self._load_current_channel_videos()
-        self._currentVideoIndex = 0
-        return self.get_current_channel()
+        return -1
+    
+    def get_videos(self):
+        return self._videos
 
 
 # Retrieves the number of milliseconds since power up
 def get_timestamp():
-    return int(round(time_.time() * 1000))
+    return int(round(time_.time() * 1_000))
 
 # Stops the VLC player
 def stop_vlc_player():
-    global VlcPlayer
-    VlcPlayer.stop()
-
-# Helper routine to play the current video
-def play_vlc_current_video():
-    global Videos
-    global VlcInstance
-    global VlcPlayer
-    stop_vlc_player()
-    display_current_video(Videos.get_current_channel(), Videos.get_current_video_name())
-    media = VlcInstance.media_new_path(Videos.get_current_video())
-    VlcPlayer.set_media(media)
-    VlcPlayer.play()
+    global VlcMediaListPlayer
+    VlcMediaListPlayer.stop()
+    VlcMediaListPlayer.release()
 
 # Handles SIGINT signal (CTRL+C) and exits the script
 def signal_handler(sig, frame):
@@ -108,25 +81,12 @@ def shutdown_signal_callback(channel):
         print("Shutting Down...")
         os.system("sudo shutdown -h now")
 
-# Displays the current channel and video name on screen
-def display_current_video(channel, video):
-    os.system("clear")
-    print("")
-    print("")
-    print("")
-    print(f"{channel}")
-    print("")
-    print(f"{video}")
-    print("")
-    print("")
-    print("")
-    time_.sleep(1.5)
-
 # Right VCR button
 # Long press (2 seconds or more) advances to next channel
 # Short press (less than 2 seconds) advances to next video
 def right_vcr_button_callback(channel):
     global RightButtonPressedStartTime
+    global VlcMediaListPlayer
     global Videos
 
     if not GPIO.input(RIGHT_VCR_BUTTON_GPIO):
@@ -135,20 +95,19 @@ def right_vcr_button_callback(channel):
     else:
         # Calculate time of right button release
         now = get_timestamp()
-        if (now - RightButtonPressedStartTime >= 2000):
+        if (now - RightButtonPressedStartTime >= 2_000):
             # Right button pressed and released for 2 seconds or more
-            Videos.next_channel()
+            video_index = Videos.next_channel()
+            VlcMediaListPlayer.play_item_at_index(video_index)
         else:
-            Videos.next_video()
-        play_vlc_current_video()
+            VlcMediaListPlayer.next()
 
 # Left VCR button
 # Long press (1 seconds or more) rewind current video 10s
 # Short press (less than 1 second) pause/resume playback
 def left_vcr_button_callback(channel):
     global LeftButtonPressedStartTime
-    global Videos
-    global VlcPlayer
+    global VlcMediaListPlayer
 
     if not GPIO.input(LEFT_VCR_BUTTON_GPIO):
         # Start timer
@@ -157,20 +116,12 @@ def left_vcr_button_callback(channel):
         # Calculate time of left button release
         now = get_timestamp()
 
-        if (now - LeftButtonPressedStartTime >= 1000):
+        if (now - LeftButtonPressedStartTime >= 1_000):
             # Left button pressed and released for 1 seconds or more
-            #VlcPlayer.set_time(VlcPlayer.get_time() - 10000) # Rewind 10s
-            #TODO restore the previous line
-            VlcPlayer.set_time(23 * 60000) # Forward 21mins
+            mediaPlayer = VlcMediaListPlayer.get_media_player()
+            mediaPlayer.set_time(mediaPlayer.get_time() - 10_000) # Rewind 10s
         else:
-            VlcPlayer.pause() # Pause/Resume playback
-
-# Reached end of video, advance to next
-def end_of_video_reached_callback(event):
-    global Videos
-    Videos.next_video()
-    play_vlc_current_video()
-
+            VlcMediaListPlayer.pause() # Pause/Resume playback
 
 SHUTDOWN_SIGNAL_GPIO  = 11
 RIGHT_VCR_BUTTON_GPIO = 25
@@ -178,13 +129,12 @@ LEFT_VCR_BUTTON_GPIO  = 26
 
 RightButtonPressedStartTime = 0
 LeftButtonPressedStartTime = 0
-Videos = VideoSelector('/home/pi/simpsonstv/videos/', '.mkv')
-VlcInstance = vlc.Instance(['--quiet', '--file-logging', '--logfile=/home/pi/simpsonstv/vlc.log','--logmode=text', '--log-verbose=3'])
-VlcPlayer = VlcInstance.media_player_new()
 
-# Set up VLC media player end reached callback
-VlcEventManager = VlcPlayer.event_manager()
-VlcEventManager.event_attach(vlc.EventType.MediaPlayerEndReached, end_of_video_reached_callback)
+Videos = VideoSelector('/home/pi/simpsonstv/videos/', '.mkv')
+VlcInstance = vlc.Instance(['--quiet'])
+VlcMediaListPlayer = vlc.MediaListPlayer()
+VlcMediaList = VlcInstance.media_list_new(Videos.get_videos())
+VlcMediaListPlayer.set_media_list(VlcMediaList)
 
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
@@ -203,8 +153,8 @@ if __name__ == '__main__':
     GPIO.setup(LEFT_VCR_BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(LEFT_VCR_BUTTON_GPIO, GPIO.BOTH, 
             callback=left_vcr_button_callback, bouncetime=30)
-    
-    play_vlc_current_video()
-    
+
+    VlcMediaListPlayer.play()
+
     signal.signal(signal.SIGINT, signal_handler)
     signal.pause()
